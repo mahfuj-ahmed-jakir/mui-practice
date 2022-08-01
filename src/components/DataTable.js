8192bbff62e3cb0403e768a9c26df8f0c7eddaf3
Mahfuj Ahmed Jakir <mahfujahmedjakir@gmail.com>
2022-08-01 16:11:06 +0600

    update

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,42 +1,30 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-
-const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-  {
-    field: "age",
-    headerName: "Age",
-    type: "number",
-    width: 90,
-  },
-  {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
-    width: 160,
-    valueGetter: (params) => `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-  },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: 33 },
-  { id: 6, lastName: "Melisandre", firstName: "Jonh", age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-];
+import { columns, rows, pagination } from "../data/data";
 
 export default function DataTable() {
+  let [searchTerm, setSearchTerm] = useState("");
+
   return (
     <div style={{ height: 400, width: "60%", margin: "20px" }}>
-      <DataGrid rows={rows} columns={columns} pageSize={5} rowsPerPageOptions={[5]} checkboxSelection />
+      <input style={{ margin: "10px 0", padding: "5px 10px" }} type="text" placeholder="Search..." onChange={(e) => setSearchTerm(e.target.value)} />
+      <DataGrid
+        rows={rows.filter((val) => {
+          if (searchTerm === "") {
+            return val;
+          } else if (val.firstName.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return val;
+          } else if (val.lastName.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return val;
+          } else {
+            return "";
+          }
+        })}
+        columns={columns}
+        pageSize={pagination.pageSize}
+        rowsPerPageOptions={pagination.rowsPerPageOptions}
+        checkboxSelection
+      />
     </div>
   );
 }
